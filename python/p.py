6167d341ad94b7233623e30b7113120df930e2c1
ahmed151515 <ahmedarafa.200444@gmail.com>
2025-06-07 13:24:44 +0300

    .

--- python/p.py
+++ python/p.py
@@ -1,12 +1,2 @@
-import matplotlib.pyplot as plt
-plt.figure()
-points = [(1, 1), (4,1), (4,4),(1,1)]
-x,y = zip(*points)
-plt.fill(x, y, color='black')
-points = [(1, 1), (1,4), (4,4),(1,1)]
-x,y = zip(*points)
-plt.fill(x, y, color='green')
-plt.gca().set_aspect('equal', adjustable='box')
-plt.show()
-
+print("sdasd")
 
